ff0dbd8499a241a97b5e44a98d6738a93fb594f5
KoalaBear <animatrix84@gmail.com>
2023-12-11 10:23:54 +0100

    - D9S1

---
+++ Day09/Program.cs
@@ -0,0 +1,61 @@
+using Library;
+using System.Diagnostics;
+
+string title = "AdventOfCode2023 - Day 09";
+Console.Title = title;
+ConsoleEx.WriteLine(title, ConsoleColor.Green);
+
+List<string> inputLines = [.. (await File.ReadAllLinesAsync("input.txt"))];
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+List<List<int>> numbers = [];
+
+foreach (string input in inputLines)
+{
+	numbers.Add(input.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToList());
+}
+
+int star1 = numbers.Select(GetExtrapolatedValue).Sum();
+
+// Answer: 
+ConsoleEx.WriteLine($"Star 1. {TimerHelper.GetMilliseconds(stopwatch):n2}ms. Answer: {star1}", ConsoleColor.Yellow);
+
+stopwatch.Restart();
+
+int star2 = -1;
+
+// Answer: 
+ConsoleEx.WriteLine($"Star 2. {TimerHelper.GetMilliseconds(stopwatch):n2}ms. Answer: {star2}", ConsoleColor.Yellow);
+
+ConsoleEx.WriteLine("END", ConsoleColor.Green);
+Console.ReadKey();
+
+// Might not be the fastest, but it's OK
+static int GetExtrapolatedValue(List<int> puzzle)
+{
+	List<List<int>> answer = [];
+
+	answer.Add(puzzle);
+
+	while (!answer.Last().All(x => x == 0))
+	{
+		List<int> innerAnswer = [];
+
+		List<int> list = answer.Last();
+
+		for (int i = 0; i < list.Count - 1; i++)
+		{
+			innerAnswer.Add(list[i + 1] - list[i]);
+		}
+
+		answer.Add(innerAnswer);
+	}
+
+	for (int i = answer.Count - 2; i >= 0; i--)
+	{
+		answer[i].Add(answer[i + 1].Last() + answer[i].Last());
+	}
+
+	return answer[0].Last();
+}
