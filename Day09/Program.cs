8dc32c801164907c937b7a28bbb41cab4f17d900
KoalaBear <animatrix84@gmail.com>
2023-12-11 10:36:31 +0100

    - D9S2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -16,23 +16,23 @@ foreach (string input in inputLines)
 	numbers.Add(input.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse).ToList());
 }
 
-int star1 = numbers.Select(GetExtrapolatedValue).Sum();
+int star1 = numbers.Select(GetExtrapolatedValueForward).Sum();
 
-// Answer: 
+// Answer: 1992273652
 ConsoleEx.WriteLine($"Star 1. {TimerHelper.GetMilliseconds(stopwatch):n2}ms. Answer: {star1}", ConsoleColor.Yellow);
 
 stopwatch.Restart();
 
-int star2 = -1;
+int star2 = numbers.Select(GetExtrapolatedValueBackward).Sum();
 
-// Answer: 
+// Answer: 769450174 (Too High)
 ConsoleEx.WriteLine($"Star 2. {TimerHelper.GetMilliseconds(stopwatch):n2}ms. Answer: {star2}", ConsoleColor.Yellow);
 
 ConsoleEx.WriteLine("END", ConsoleColor.Green);
 Console.ReadKey();
 
 // Might not be the fastest, but it's OK
-static int GetExtrapolatedValue(List<int> puzzle)
+static int GetExtrapolatedValueForward(List<int> puzzle)
 {
 	List<List<int>> answer = [];
 
@@ -58,4 +58,32 @@ static int GetExtrapolatedValue(List<int> puzzle)
 	}
 
 	return answer[0].Last();
+}
+
+static int GetExtrapolatedValueBackward(List<int> puzzle)
+{
+	List<List<int>> answer = [];
+
+	answer.Add(puzzle);
+
+	while (!answer.Last().All(x => x == 0))
+	{
+		List<int> innerAnswer = [];
+
+		List<int> list = answer.Last();
+
+		for (int i = 0; i < list.Count - 1; i++)
+		{
+			innerAnswer.Add(list[i + 1] - list[i]);
+		}
+
+		answer.Add(innerAnswer);
+	}
+
+	for (int i = answer.Count - 2; i >= 0; i--)
+	{
+		answer[i].Insert(0, answer[i].First() - answer[i + 1].First());
+	}
+
+	return answer[0].First();
 }
